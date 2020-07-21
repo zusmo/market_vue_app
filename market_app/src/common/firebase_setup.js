4c537f2e2549d4154723527dfa31b7a84facc975
zusmo <josue.leoz10@gmail.com>
2020-07-20 23:20:43 -0600

    Firebase and Bootstrap added

---
+++ market_app/src/common/firebase_setup.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCu1DH7WkyDNIWhElfluSSi6oG__nYP6PA",
+    authDomain: "marketapp-278e2.firebaseapp.com",
+    databaseURL: "https://marketapp-278e2.firebaseio.com",
+    projectId: "marketapp-278e2",
+    storageBucket: "marketapp-278e2.appspot.com",
+    messagingSenderId: "387499543848",
+    appId: "1:387499543848:web:f0db60af46d3407a35141d"
+  };
+
+  export default firebase.initializeApp(firebaseConfig); 
